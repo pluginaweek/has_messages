f6c4990ee5c1ce7a66175633f71c74cbc9f54b32
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2006-12-31 22:41:20 +0000

    Fixed formatting. Made unique indexes more human-readable.

--- db/migrate/001_create_messages.rb
+++ db/migrate/001_create_messages.rb
@@ -1,18 +1,17 @@
 class CreateMessages < ActiveRecord::Migration
-  
   class Message < ActiveRecord::Base
     acts_as_state_machine :initial => :dummy
   end
   
   def self.up
     create_table :messages do |t|
-      t.column :from_id,                :integer,   :unsigned => true, :references => nil
-      t.column :recipient_id,           :integer,   :unsigned => true, :references => nil
-      t.column :reference_message_id,   :integer,   :unsigned => true, :references => :messages
-      t.column :subject,                :text,      :null => false
-      t.column :body,                   :text,      :null => false
-      t.column :created_at,             :datetime,  :null => false
-      t.column :type,                   :string,    :null => false
+      t.column :from_id,              :integer,   :unsigned => true, :references => nil
+      t.column :recipient_id,         :integer,   :unsigned => true, :references => nil
+      t.column :reference_message_id, :integer,   :unsigned => true, :references => :messages
+      t.column :subject,              :text,      :null => false
+      t.column :body,                 :text,      :null => false
+      t.column :created_at,           :datetime,  :null => false
+      t.column :type,                 :string,    :null => false
     end
     
     Message::State.migrate_up
--- db/migrate/002_create_message_recipients.rb
+++ db/migrate/002_create_message_recipients.rb
@@ -1,14 +1,14 @@
 class CreateMessageRecipients < ActiveRecord::Migration
   def self.up
     create_table :message_recipients do |t|
-      t.column :messageable_id,   :integer, :unsigned => true, :references => nil
-      t.column :message_id,       :integer, :null => false, :unsigned => true
-      t.column :kind,             :string,  :null => false, :default => 'to'
-      t.column :position,         :integer, :null => false
-      t.column :type,             :string,  :null => false
+      t.column :messageable_id, :integer, :unsigned => true, :references => nil
+      t.column :message_id,     :integer, :null => false, :unsigned => true
+      t.column :kind,           :string,  :null => false, :default => 'to'
+      t.column :position,       :integer, :null => false
+      t.column :type,           :string,  :null => false
     end
     add_index :message_recipients, [:message_id, :messageable_id, :type], :unique => true, :name => 'unique_message_recipients'
-    add_index :message_recipients, [:message_id, :kind, :position], :unique => true
+    add_index :message_recipients, [:message_id, :kind, :position], :unique => true, :name => 'unique_message_recipient_positions'
   end
   
   def self.down
